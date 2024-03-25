39d396d38d152cdfffb1eea9b94b6b424902b83d
QuachHoangBo <121910981+QuachHoangBo@users.noreply.github.com>
2024-03-25 16:57:29 +0700

    working

--- config/db.js
+++ config/db.js
@@ -1,20 +1,19 @@
 const mongoose = require("mongoose");
 mongoose.set("strictQuery", true);
 
-const local = "mongodb://localhost:27017/MyDB";
-
-const atlat =
-  "mongodb+srv://boqhpd08480:0356145104Az%40@cluster0.qchgngp.mongodb.net/MyDB?retryWrites=true&w=majority&appName=Cluster0";
+const atlasURI =
+  "mongodb+srv://bophpd08480:MvtjNCqlC2cvMmP3@cluster0.unftlza.mongodb.net/MyDBretryWrites=true&w=majority&appName=Cluster0";
+const local = "mongodb://localhost:27017/database";
 const connect = async () => {
   try {
-    await mongoose.connect(atlat, {
+    await mongoose.connect(local, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     });
     console.log("Connect success");
   } catch (error) {
-    console.log("Connect Fail");
-    console.log(error);
+    console.log("Connect fail:", error.message);
   }
 };
+
 module.exports = { connect };
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,30 +1,125 @@
 var express = require("express");
 var router = express.Router();
-const modelUser = require("../model/user");
+const modeUser = require("../models/user");
+const Transporter = require("../config/mail");
+const Upload = require("../config/upload");
 
-/* Get user listing*/
-
-router.get("/test", function (req, res, next) {
-  res.send("respond with a resource test");
+router.get("/test", function (reg, res, next) {
+  res.send("respond with a resource user test");
 });
 
 //add data
-router.post("/add", async (req, res) => {
+
+router.post("/add", Upload.single("avater"), async (req, res) => {
   try {
-    const model = new modelUser(req.body);
-    const result = await model.save();
-    res.send(result);
+    const { file } = req;
+    const urlImages = `${req.protocol}://${req.get("host")}/uploads/${
+      file.fieldname
+    }`;
+    const model = new modeUser(req.body);
+    model.avater = urlImages;
+    const result = await model.save(); //thêm  dl vaò đata
+    if (result) {
+      const mailOption = {
+        from: "boqhpd08480@fpt.edu.vn",
+        to: model.email, //mail người dùng
+        subject: "wellcom to nodejs",
+        text: "chúc mừng bạn đã đăng ký thành công",
+      };
+      await Transporter.sendMail(mailOption);
+      res.json({
+        status: 200,
+        message: "Thêm thành công",
+        data: result,
+      });
+    } else {
+      res.json({
+        status: 400,
+        message: "Thêm thất bại",
+        data: result,
+      });
+    }
+    // res.send(result)
   } catch (error) {
-    res.send(error);
+    console.log(error);
   }
 });
-
+//
 router.get("/list", async (req, res) => {
-  const result = await modelUser.find();
+  const result = await modeUser.find({});
   try {
     res.send(result);
   } catch (error) {
-    res.send(error);
+    console.log(error);
+  }
+});
+//
+router.get("/getbyid/:id", async (req, res) => {
+  const result = await modeUser.findById(req.params.id);
+  try {
+    if (result) {
+      res.send(result);
+    } else {
+      res.json({
+        status: 400,
+        message: "không tìm thấy ID  ",
+        data: result,
+      });
+    }
+  } catch (error) {
+    if (error.name === "CastError") {
+      res.status(404).send("Invalid ID format");
+    } else {
+      console.log(error);
+      res.status(500).send("Internal Server");
+    }
+  }
+});
+router.patch("/edit/:id", async (req, res) => {
+  const result = await modeUser.findByIdAndUpdate(req.params.id, req.body);
+  try {
+    if (result) {
+      const rs = await result.save();
+      res.send(rs);
+    } else {
+      res.json({
+        status: 400,
+        message: "không tìm thấy ID  ",
+        data: result,
+      });
+    }
+  } catch (error) {
+    if (error.name === "CastError") {
+      res.status(404).send("Invalid ID format");
+    } else {
+      console.log(error);
+      res.status(500).send("Internal Server");
+    }
+  }
+});
+router.delete("/delete/:id", async (req, res) => {
+  const result = await modeUser.findByIdAndDelete(req.params.id, req.body);
+  try {
+    if (result) {
+      res.json({
+        status: 200,
+        message: "Xóa thành công  ",
+        data: result,
+      });
+    } else {
+      res.json({
+        status: 400,
+        message: "Xóa thất bại  ",
+        data: [],
+      });
+    }
+  } catch (error) {
+    if (error.name === "CastError") {
+      res.status(404).send("Invalid ID format");
+    } else {
+      console.log(error);
+      res.status(500).send("Internal Server");
+    }
   }
 });
 
